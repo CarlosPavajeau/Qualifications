6b7548006aaf19cc8d102e74e36f6ada2a3817b1
Cantte <cantte098@gmail.com>
2020-10-10 11:33:01 -0500

    feat: Make application database and update models

--- app/src/main/java/com/qualifications/model/Qualification.kt
+++ app/src/main/java/com/qualifications/model/Qualification.kt
@@ -12,19 +12,32 @@ class Qualification {
 
     var activities: ArrayList<Activity> = ArrayList()
 
-    val total: Float
+    private val totalPartial: Float
     get() {
         return activities.map { a -> a.percent * a.note }.reduce { acc, fl -> acc + fl }
     }
 
+    val total: Float
+    get() {
+        return totalPartial * totalPercent
+    }
 
     private val totalPercent: Float
+    get() {
+        return when (cort) {
+            1, 2 -> 0.3F
+            3 -> 0.4F
+            else -> 0.0F
+        }
+    }
+
+    private val totalActivitiesPercent: Float
     get() {
         return activities.map { a -> a.percent }.reduce { acc, fl -> acc + fl }
     }
 
     fun addActivity(activity: Activity): Boolean {
-        if (totalPercent + activity.percent > MAX_PERCENT)
+        if (totalActivitiesPercent + activity.percent > MAX_PERCENT)
             return false
 
         activities.add(activity)
--- app/src/main/java/com/qualifications/model/Subject.kt
+++ app/src/main/java/com/qualifications/model/Subject.kt
@@ -3,20 +3,21 @@ package com.qualifications.model
 class Subject(val code: String, val name: String) {
     val qualifications: ArrayList<Qualification> = ArrayList(3)
 
-    val definitive: Float
-    get() {
-        var accumulate = 0.0f
-        qualifications.forEach { accumulate += it.total }
-        return accumulate / qualifications.count()
+    init {
+        var cort = 1
+        qualifications.forEach { it.cort = cort++ }
     }
 
-    /**
-     * Add a qualification to the subject with specific cort
-     * */
-    fun addQualification(qualification: Qualification) {
-        if (qualification.cort > 3 || qualification.cort <= 0)
-            return
+    val definitive: Float
+    get() {
+        return qualifications.map { q -> q.total }.reduce { acc, fl -> acc + fl }
+    }
 
-        qualifications[qualification.cort] = qualification
+    fun addQualification(qualification: Qualification): Boolean {
+        if (qualification.cort > 3 || qualification.cort <= 0)
+            return false
+
+        qualifications[qualification.cort - 1] = qualification
+        return true
     }
 }
