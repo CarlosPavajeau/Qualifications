d7c5c122f43a2369c4d9505e4ab7c0cd33dfb1e6
Cantte <cantte098@gmail.com>
2020-10-10 16:21:59 -0500

    feat: Add RegisterActivityFragment, and make others fragments

---
+++ app/src/main/java/com/qualifications/view/ui/fragments/RegisterActivityFragment.kt
@@ -0,0 +1,23 @@
+package com.qualifications.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qualifications.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterActivityFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterActivityFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_activity, container, false)
+    }
+}
